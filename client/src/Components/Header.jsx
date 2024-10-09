a3b9b5b3034100f50fb469e9b18befa96023abd5
Gyandeep Yadav <yadavgyan2002@gmail.com>
2024-10-09 21:49:16 +0530

    complete signup page funcations

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-transparent shadow-md'>
+    <header className='bg-black shadow-md '>
     <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
-      <Link to='/'>
+      <Link to='/'> 
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <span className="text-slate-500">GD</span>
         <span className="text-slate-700">ESTATE</span>
@@ -19,19 +19,19 @@ export default function Header() {
 
       <ul className="flex gap-6">
   <Link to='/'>
-    <li className="relative text-slate-950 rounded-xl pl-5 pr-5 group">
+    <li className="relative text-white rounded-xl pl-5 pr-5 group">
       Home
       <span className="absolute inset-0 w-full h-full bg-red-600 rounded-xl z-[-1] scale-0 opacity-0 transition-transform duration-500 group-hover:scale-100 group-hover:opacity-100"></span>
     </li>
   </Link>
   <Link to='/about'>
-    <li className="relative text-slate-950 rounded-xl pl-5 pr-5 group">
+    <li className="relative text-white rounded-xl pl-5 pr-5 group">
       About
       <span className="absolute inset-0 w-full h-full bg-red-600 rounded-xl z-[-1] scale-0 opacity-0 transition-transform duration-500 group-hover:scale-100 group-hover:opacity-100"></span>
     </li>
   </Link>
   <Link to='/sign-in'>
-    <li className="relative text-slate-950 rounded-xl pl-5 pr-5 group">
+    <li className="relative text-white rounded-xl pl-5 pr-5 group">
       Sign-in
       <span className="absolute inset-0 w-full h-full bg-red-600 rounded-xl z-[-1] scale-0 opacity-0 transition-transform duration-500 group-hover:scale-100 group-hover:opacity-100"></span>
     </li>
--- client/src/Pages/signup.jsx
+++ client/src/Pages/signup.jsx
@@ -1,7 +1,53 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../css/Signin.css'
+import { useState } from 'react';
 
-export default function signup() {
+export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [ error,setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value,
+
+      }
+    );
+
+  };
+  
+  const handleSubmit =async (e) =>{
+    e.preventDefault();
+    try{
+      
+    
+    setLoading(true);
+    const res = await fetch('/api/auth/signup',
+      {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(formData),
+      }
+     );
+     const data = await res.json();
+     if(data.success === false){
+      setError(data.message);
+      setLoading(false);
+      return;
+     }
+     setLoading(false);
+     console.log(data);
+     setError(null);
+     navigate('/sign-in');
+    }catch (error){
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className='main-div-1'>
     <div className="main-div-2">
@@ -14,20 +60,23 @@ export default function signup() {
         <input type="checkbox" id="chk" aria-hidden="true" />
 
         <div className="signup">
-          <form className='form'>
+          <form onSubmit={handleSubmit} className='form'>
             <label htmlFor="chk" aria-hidden="true">
               Sign up
             </label>
-            <input className='signup-input' type="text" name="username" placeholder="User name" id='username' required />
-            <input className='signup-input' type="email" name="email" placeholder="Email" id='email' required />
+            <input className='signup-input' type="text" name="username" placeholder="User name" id='username'  onChange={handleChange}/>
+            <input className='signup-input' type="email" name="email" placeholder="Email" id='email'  onChange={handleChange}/>
             {/* <input className='signup-input' type="number" name="phone" placeholder="Phone number" id='phone' required /> */}
-            <input className='signup-input' type="password" name="password" placeholder="Password" id='password' required />
-            <button className='signup-button' type="submit">Sign up</button>
+            <input className='signup-input' type="password" name="password" placeholder="Password" id='password'  onChange={handleChange}/>
+            <button disabled={loading} className='signup-button' type="submit">
+              {loading ? 'Loading...':'Sign Up'}
+            </button>
           </form>
           <div className='flex gap-2 mt-5 flex justify-center'>
             <p>Have an Account?</p>
             <Link to='/Sign-in'><span className='sign-in-link '>Sign in</span></Link>
           </div>
+          {error && <p className='text-red-500 mt-5'>{error}</p>}
         </div>
 
         {/* <div className="login">
